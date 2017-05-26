b2b36288c84acb61c5a10fad51bf7d7d9d5175e8
Felipe Machado <felipe.machado1992@hotmail.com>
2017-05-26 04:07:40 -0300

    Criação do pacote DAO e da classe UsuarioDao

---
+++ Projeto06-Grupo5-WebQuiz/src/java/java/br/com/fatecpg/dao/UsuarioDao.java
@@ -0,0 +1,28 @@
+package java.br.com.fatecpg.dao;
+
+import java.br.com.fatecpg.quiz.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*@author Felipe*/
+public class UsuarioDao {
+    
+    private Connection connection;
+    
+    
+    public void insereUsuario(Usuario usuario) throws SQLException{
+        String sql ="INSERT INTO USUARIO (NM_USUARIO, SENHA, LOGIN)";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1,usuario.getNome());
+            stmt.setString(2,usuario.getSenha());
+            stmt.setString(3,usuario.getLogin());
+            
+            stmt.execute();
+            stmt.close();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }    
+}
