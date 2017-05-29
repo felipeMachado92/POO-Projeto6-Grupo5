9cc94cfcc3c98d2623ad0c1926b064db888dbc07
Felipe Machado <felipe.machado1992@hotmail.com>
2017-05-29 01:07:31 -0300

    Criação e encapsulamento das variaveis

--- Projeto06-Grupo5-WebQuiz/src/java/java/br/com/fatecpg/quiz/Questao.java
+++ Projeto06-Grupo5-WebQuiz/src/java/java/br/com/fatecpg/quiz/Questao.java
@@ -3,6 +3,34 @@ package java.br.com.fatecpg.quiz;
 
 /*@author Felipe*/
 public class Questao {
-        String textoQuestao;
+        private int idQuestao;
+        private String textoQuestao;
+        private Teste teste;
+
+    public int getIdQuestao() {
+        return idQuestao;
+    }
+
+    public void setIdQuestao(int idQuestao) {
+        this.idQuestao = idQuestao;
+    }
+
+    public String getTextoQuestao() {
+        return textoQuestao;
+    }
+
+    public void setTextoQuestao(String textoQuestao) {
+        this.textoQuestao = textoQuestao;
+    }
+
+    public Teste getTeste() {
+        return teste;
+    }
+
+    public void setTeste(Teste teste) {
+        this.teste = teste;
+    }
+        
+        
         
 }
