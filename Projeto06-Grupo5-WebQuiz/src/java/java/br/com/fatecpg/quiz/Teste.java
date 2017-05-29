bf67a5631e4b51d34dea2bf32bc7917c80779577
Felipe Machado <felipe.machado1992@hotmail.com>
2017-05-29 00:11:25 -0300

    Criação das Variaveis da classe teste

--- Projeto06-Grupo5-WebQuiz/src/java/java/br/com/fatecpg/quiz/Teste.java
+++ Projeto06-Grupo5-WebQuiz/src/java/java/br/com/fatecpg/quiz/Teste.java
@@ -2,5 +2,41 @@ package java.br.com.fatecpg.quiz;
 
 /* @author Felipe*/
 public class Teste {
+        private int idTeste;
+        private String nomeTeste;
+        private String descTeste;
+        private Questao questao;
+
+    public int getIdTeste() {
+        return idTeste;
+    }
+
+    public void setIdTeste(int idTeste) {
+        this.idTeste = idTeste;
+    }
+
+    public String getNomeTeste() {
+        return nomeTeste;
+    }
+
+    public void setNomeTeste(String nomeTeste) {
+        this.nomeTeste = nomeTeste;
+    }
+
+    public String getDescTeste() {
+        return descTeste;
+    }
+
+    public void setDescTeste(String descTeste) {
+        this.descTeste = descTeste;
+    }
+
+    public Questao getQuestao() {
+        return questao;
+    }
+
+    public void setQuestao(Questao questao) {
+        this.questao = questao;
+    }
         
 }
